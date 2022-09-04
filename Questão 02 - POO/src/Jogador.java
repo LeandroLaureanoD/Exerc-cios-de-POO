39defa172f8efcbbec71bec4f10c1d9870618b6e
LeandroLaureanoD <57117906+LeandroLaureanoD@users.noreply.github.com>
2022-09-04 14:27:23 -0300

    Atualização

---
+++ Questão 02 - POO/src/Jogador.java
@@ -0,0 +1,77 @@
+public class Jogador{
+    private int id;
+    private String nome;
+    private String apelido;
+    private String dataNascimento;
+    private int numero;
+    private String posicao;
+    private int qualidade;
+
+    //CONNSTRUTOR CLASE JOGADOR
+    public Jogador(int id, String nome, String apelido, String dataNascimento, int numero, String posicao, int qualidade) {
+        this.id = id;
+        this.nome = nome;
+        this.apelido = apelido;
+        this.dataNascimento = dataNascimento;
+        this.numero = numero;
+        this.posicao = posicao;
+        this.qualidade = qualidade;
+    }
+
+    //GETTER E SETTER
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public int getQualidade() {
+        return qualidade;
+    }
+
+    public void setQualidade(int qualidade) {
+        this.qualidade = qualidade;
+    }
+}
---
+++ Questão 02 - POO/src/Time.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+public class Time {
+    private String nome;
+    private String apelido;
+    private String fundacao;
+    public ArrayList<Jogador> plantel;
+    public ArrayList<Jogador> relacionadosList;
+
+    public Time(){
+    }
+
+    //CONTRUTOR CLASSE TIME
+    public Time(String nome, String apelido, String fundacao, ArrayList<Jogador> plantel, ArrayList<Jogador> relacionadosList) {
+        this.nome = nome;
+        this.apelido = apelido;
+        this.fundacao = fundacao;
+        this.plantel = plantel;
+        this.relacionadosList = relacionadosList;
+    }
+
+    // GETTER E SETTER
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getFundacao() {
+        return fundacao;
+    }
+
+    public void setFundacao(String fundacao) {
+        this.fundacao = fundacao;
+    }
+
+    public ArrayList<Jogador> getPlantel() {
+        return plantel;
+    }
+
+    public void setPlantel(ArrayList<Jogador> plantel) {
+        this.plantel = plantel;
+    }
+
+    public ArrayList<Jogador> getRelacionadosList() {
+        return relacionadosList;
+    }
+
+    public void setRelacionadosList(ArrayList<Jogador> relacionadosList) {
+        this.relacionadosList = relacionadosList;
+    }
+
+    //METODOS
+    public ArrayList<Jogador> relacionarJogadores (){
+        List jogadores = plantel.stream().sorted(Comparator.comparing(Jogador::getQualidade).reversed()).limit(11).collect(Collectors.toList());
+        relacionadosList = new ArrayList<Jogador>(jogadores);
+        return relacionadosList;
+
+    }
+
+    /*public String toString(){
+        return getNome() + apelido + fundacao + plantel + relacionadosList;
+    }*/
+}
