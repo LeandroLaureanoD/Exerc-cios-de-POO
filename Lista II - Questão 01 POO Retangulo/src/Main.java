a1147d11507e58ee52b3d67c482b3d9330ade2fc
LeandroLaureanoD <57117906+LeandroLaureanoD@users.noreply.github.com>
2022-09-11 13:16:37 -0300

    Update Condicao While

--- Lista II - Questão 01 POO Retangulo/src/Main.java
+++ Lista II - Questão 01 POO Retangulo/src/Main.java
@@ -13,22 +13,18 @@ public class Main {
 
         while (condicao == 's'){
 
-            if(condicao == 's'){
-                System.out.println("Digte o valor do lado 1 do Retangulo: ");
-                ladoUm = sc.nextFloat();
-                retangulo.setLado1(ladoUm);
+            System.out.println("Digte o valor do lado 1 do Retangulo: ");
+            ladoUm = sc.nextFloat();
+            retangulo.setLado1(ladoUm);
 
-                System.out.println("Digte o valor do lado 2 do Retangulo: ");
-                ladoDois = sc.nextFloat();
-                retangulo.setLado2(ladoDois);
+            System.out.println("Digte o valor do lado 2 do Retangulo: ");
+            ladoDois = sc.nextFloat();
+            retangulo.setLado2(ladoDois);
 
-                retangulo.calcularArea();
-                retangulo.calcularPerimetro();
+            retangulo.calcularArea();
+            retangulo.calcularPerimetro();
 
-                System.out.println(retangulo);
-            }else {
-                sc.close();
-            }
+            System.out.println(retangulo);
             System.out.println("Deseja calcular outro Retangulo? (s/n) ");
             condicao = sc.next().charAt(0);
         }
